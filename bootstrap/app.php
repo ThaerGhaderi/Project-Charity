90b54d925d555db5b20f28507358be1ea7466532
Thaer777 <mlkalglam@gmail.com>
2026-08-15 22:52:15 +0300

    Clean bootstrap configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
     ->withMiddleware(function (Middleware $middleware) {
+
+        // 1. استثناء مسارات الـ API بالكامل من فحص حماية الـ CSRF
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
