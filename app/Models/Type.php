bc85bf8a92f0c67b4e5ab1c245df92aa56dc19ac
Thaer777 <mlkalglam@gmail.com>
2026-08-18 11:51:56 +0300

    Fex the model Type

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
+    protected $fillable = ['name'];
     public function beneficiaries()
     {
         return $this->belongsToMany(BeneficiaryProfile::class, 'beneficiary_types');
